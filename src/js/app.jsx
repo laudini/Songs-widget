61f0cfcb02b2eb038ce08210929980ef011d52cf
Kamil <krzeminski.yt.kamil@gmail.com>
2018-08-10 14:47:48 +0200

    wip

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -32,9 +32,22 @@ class Container extends React.Component {
             currentSong : '',
             currentArtist : 'Choose your song!',
             backgroundClasses: ['duch', 'despacito', 'perfect', 'poczatek', 'wolves'],
-            activeSongBg: 'default'
+            activeSongBg: 'default',
+            activeLayer: 'player'
         }
     }
+
+    changeToPlaylist = () => {
+        this.setState({
+            activeLayer: 'playlist'
+        })
+    };
+
+    changeToPlayer = () => {
+        this.setState({
+            activeLayer: 'player'
+        })
+    };
 
     changeSong = (e) => {
       this.setState({
@@ -49,7 +62,7 @@ class Container extends React.Component {
             <div id="MainContainer">
                 <Player>
                     <div className={this.state.activeSongBg} id="PlayerTop">
-                        <PlayerOptions/>
+                        <PlayerOptions changeToPlaylist={this.changeToPlaylist}/>
                         <PlayerInfo currentSong={this.state.currentSong} currentArtist={this.state.currentArtist}/>
                     </div>
                     <div id="PlayerBottom">
@@ -58,7 +71,7 @@ class Container extends React.Component {
                 </Player>
                 <Playlist>
                     <div id="PlaylistTop">
-                        <PlaylistHeader/>
+                        <PlaylistHeader changeToPlayer={this.changeToPlayer}/>
                     </div>
                     <div id="PlaylistBottom">
                         <PlaylistBody songs={this.state.song} singers={this.state.singer} times={this.state.time} changeSong={this.changeSong}/>
@@ -93,10 +106,10 @@ class PlayerOptions extends React.Component {
     render() {
         return (
             <div id="PlayerOptions">
-                <button id="redo"></button>
-                <button id="shuffle"></button>
-                <button id="repeat"></button>
-                <button id="hamburger"></button>
+                <button id="Redo"></button>
+                <button id="Shuffle"></button>
+                <button id="Repeat"></button>
+                <button id="Hamburger"></button>
 
             </div>
         )
@@ -128,11 +141,11 @@ class PlayerButtons extends React.Component {
     render() {
         return (
             <div id="PlayerButtons">
-                <button id="share"></button>
-                <button id="previous"></button>
-                <button id="play"></button>
-                <button id="next"></button>
-                <button id="like"></button>
+                <button id="Share"></button>
+                <button id="Previous"></button>
+                <button id="Play"></button>
+                <button id="Next"></button>
+                <button id="Like"></button>
             </div>
         )
     }
@@ -142,11 +155,14 @@ class PlayerButtons extends React.Component {
 class Playlist extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            display : "noDisplay"
+        }
     }
 
     render() {
         return (
-            <div id="PlaylistContainer">
+            <div className={this.state.display} id="PlaylistContainer">
                 {this.props.children}
             </div>
         )
@@ -181,12 +197,21 @@ class PlaylistBody extends React.Component {
 
             for (let i = 0; i < this.props.songs.length; i++) {
 
-                li.push(<li id={i} onClick={this.props.changeSong}>
-                    <div>
-                        <div>{this.props.times[i]}</div>
-                        <div>{this.props.singers[i]}</div>
+                li.push(<li className="SongTiles" id={i} onClick={this.props.changeSong}>
+                    <div className="divContainer">
+                    <div className="FlexDiv">
+                    <div className="SongTilesInfoTop">
+                        <div className="SongTime">{this.props.times[i]} |</div>
+                        <div className="SongAuthor"> {this.props.singers[i]}</div>
                     </div>
-                    <div>{this.props.songs[i]}</div>
+                    <div className="SongName">{this.props.songs[i]}</div>
+                    </div>
+                    <div className="PlaylistButtons">
+                        <button className="PlaylistShare"></button>
+                        <button className="PlaylistLike"></button>
+                    </div>
+                    </div>
+                    <hr/>
                 </li>)
             }
 
